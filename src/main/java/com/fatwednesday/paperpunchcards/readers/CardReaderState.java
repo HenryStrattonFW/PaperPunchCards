e16652e751f6aa04d5c9e7134240282aa3d45a5e
HenryStrattonFW <henrystrattonfw@gmail.com>
2025-07-20 16:28:49 +1000

    Adding missing file from previous commit for card reader state.

---
+++ src/main/java/com/fatwednesday/paperpunchcards/readers/CardReaderState.java
@@ -0,0 +1,25 @@
+package com.fatwednesday.paperpunchcards.readers;
+
+import net.minecraft.util.StringRepresentable;
+import org.jetbrains.annotations.NotNull;
+
+public enum CardReaderState implements StringRepresentable
+{
+    UNSET("unset"),
+    EMPTY("empty"),
+    BAD("bad"),
+    GOOD("good");
+
+    private final String name;
+
+    CardReaderState(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public @NotNull String getSerializedName()
+    {
+        return this.name;
+    }
+}
